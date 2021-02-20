e35019b9f7600aa68bd6a121c794d4ad693d5a83
Nima Mohammadi <nimamo@vt.edu>
2021-02-20 18:31:35 -0500

    Prepare for batch execution

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import numpy as np
 import ANNarchy as AN
+AN.clear()
 from pops import create_populations
 from projs import create_projections
 import random
+import pickle
 
+def gen_filename(params):
+    out = ('lambda[{lambda_}]_Wscale[{W_Scale}]_D[{delay}]_liqinpS[{liq_inp_size}]' +
+           '_Winp[{W_inp}]_Dinp[{delay_inp}]_EIr[{exc_inh_ratio}]_Geom[{liquid_geometry}]' + 
+         '_inpS[{inp_size}]_readoutS[{readout_size}]').format(**params)
+    out = out.replace('(','').replace(')','').replace(' ', '')
+    return out
 
 
 ####################################### PARAMETERS
 
 params = {
-    'delay': (1, 3),
-    
-    'W_inp': (4, 7), # Could be tuple
-    
-    'delay_inp': 2,
-    
+    'delay': (1, 20),
+    'W_inp': (4, 8),
+    'delay_inp': (1, 20),
+
     'liquid_geometry': (5, 5, 8),
     'exc_inh_ratio': .8,
     'inp_size': 1,
-    'liq_inp_size': 3,
+    'liq_inp_size': 10,
     'readout_size': 5,
-    'lambda_': 3,
-    'W_Scale': 10.2,
+    'lambda_': 3.5,
+    'W_Scale': 7,
 
     'dt': .1,
     'seed': 1337
 }
+
+output_filename = gen_filename(params) + '.out'
 
 
 np.random.seed(params['seed'])
@@ -37,43 +45,23 @@ AN.setup(dt=params['dt'], seed=params['seed'])
 
 network = create_populations(params)
 
-
-print("#Nodes in RC: {},  Exc-Inh ratio: {},  #Exc: {},  #Inp: {}".format(network['liquid_pop'].size, 
-                                        params['exc_inh_ratio'], network['liquid_exc'].size, params['inp_size']))
+#print("#Nodes in RC: {},  Exc-Inh ratio: {},  #Exc: {},  #Inp: {}".format(network['liquid_pop'].size, 
+                                        #params['exc_inh_ratio'], network['liquid_exc'].size, params['inp_size']))
 
 
 ######################################## NETWORK PARAMETERS
 
 proj_params = {
-    'U_ee': 0.5, #STP params
-    'U_ei': 0.05,
-    'U_ie': 0.25,
-    'U_ii': 0.32,
-    
-    'tau_rec_ee': 1.1,
-    'tau_rec_ei': 0.125,
-    'tau_rec_ie': 0.7,
-    'tau_rec_ii': 0.144,
-    
-    'tau_facil_ee': 0.05,
-    'tau_facil_ei': 1.2,
-    'tau_facil_ie': 0.02,
-    'tau_facil_ii': 0.06,
-    
-    'W_ee': .3 * params['W_Scale'],
-    'W_ei': .6 * params['W_Scale'],
-    'W_ie': .19 * params['W_Scale'],
-    'W_ii': .19 * params['W_Scale'],
-    
-    'C_ee': .3,
-    'C_ei': .2,
-    'C_ie': .4,
-    'C_ii': .1,
+    'W_ee': .3 * params['W_Scale'], 'W_ei': .6 * params['W_Scale'], 
+    'W_ie': .19 * params['W_Scale'], 'W_ii': .19 * params['W_Scale'],
+
+    'U_ee': 0.5, 'U_ei': 0.05, 'U_ie': 0.25, 'U_ii': 0.32,
+    'tau_rec_ee': 1.1, 'tau_rec_ei': 0.125, 'tau_rec_ie': 0.7, 'tau_rec_ii': 0.144,
+    'tau_facil_ee': 0.05, 'tau_facil_ei': 1.2, 'tau_facil_ie': 0.02, 'tau_facil_ii': 0.06,
+    'C_ee': .3, 'C_ei': .2, 'C_ie': .4, 'C_ii': .1,
 }
 
-
-
-print("Weights EE:{:.2f}, EI:{:.2f}, IE:{:.2f}, II:{:.2f}".format(proj_params['W_ee'], proj_params['W_ei'], proj_params['W_ie'], proj_params['W_ii']))
+#print("Weights EE:{:.2f}, EI:{:.2f}, IE:{:.2f}, II:{:.2f}".format(proj_params['W_ee'], proj_params['W_ei'], proj_params['W_ie'], proj_params['W_ii']))
 
 projs = create_projections(network, params, proj_params)
 
@@ -84,16 +72,23 @@ m_readout = AN.Monitor(network['readout_pop'], 'spike')
 AN.compile(compiler_flags="-march=native -O0")
 
 ######################################## SIMULATION
-network['input_pop'].spike_times = [[0]]
-AN.simulate(1000, measure_time=False)
+current_time = int(AN.get_current_step() * params['dt'])
+network['input_pop'].spike_times = [current_time + 10]
 
-readout_spikes = m_readout.get('spike')
-liquid_spikes = m_liquid.get('spike')
+liq_spikes = []
+for i in range(20):
+    AN.simulate(100, measure_time=False)
+    readout_spikes = m_readout.get('spike')
+    liquid_spikes = m_liquid.get('spike')
+    total_liquid_spikes = sum([len(_) for _ in list(liquid_spikes.values())])
+    liq_spikes.append(total_liquid_spikes)
+    print(i+1, total_liquid_spikes)
 
-print('Readout Spikes')
 
-print({kk: len(vv) for kk, vv in readout_spikes.items() if len(vv)!=0})
-print('Liquid Spikes')
-#print({kk: len(vv) for kk, vv in liquid_spikes.items() if len(vv)!=0})
 
-print()
+with open(output_filename, 'wb') as fo:
+    pickle.dump(liq_spikes, fo)
+
+##print({kk: len(vv) for kk, vv in liquid_spikes.items() if len(vv)!=0})
+
+#print()
